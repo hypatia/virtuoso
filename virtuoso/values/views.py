bd21bc691476ce67289e6a08a7a723ca84212840
Lukas Blakk <lukasblakk@gmail.com>
2014-02-19 19:26:05 -0800

    removing *.pyc and updates

--- virtuoso/values/views.py
+++ virtuoso/values/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
 
 from values.models import Value
 
 def index(request):
     values_list = Value.objects.all().order_by('-frequency')
-    context = {'values_list': values_list}
+    context = {'values_list': values_list, 'values_json': [{ "text": 'value', "size": 5}, {"text": 'value2', "size": 10}]}
     return render(request, 'values/index.html', context)
 
 def detail(request, value_id):
--- virtuoso/virtuoso/urls.py
+++ virtuoso/virtuoso/urls.py
@@ -1,15 +1,12 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
-from virtuoso import views
+from values import views as values
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', views.index, name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', values.index, name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url('', include('social.apps.django_app.urls', namespace='social')),
     url(r'^values/', include('values.urls')),
